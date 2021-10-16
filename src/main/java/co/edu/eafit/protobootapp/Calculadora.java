61c7d6ff5b14eb5c8bca461e6c624b9706e76185
Cristian Franco <cfrancobedoya@gmail.com>
2021-10-15 21:53:10 -0500

    Fixing test

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -2,7 +2,7 @@ package co.edu.eafit.protobootapp;
 
 public class Calculadora {
 
-    public Double cuadrado(Double numero) { return numero + numero; }
+    public Double cuadrado(Double numero) { return numero * numero; }
     
     public Double cubo(Double numero) {
         return numero * numero * numero;
